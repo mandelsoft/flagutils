f7ecf280b7159ee0b099b2cdae3d2f387295aec9
Uwe Krueger <uwe.krueger@goldmail.de>
2026-03-25 19:04:42 +0100

    option refs for shared options

--- option.go
+++ option.go
@@ -1,6 +1,9 @@
 package flagutils
 
 import (
+	"context"
+
+	"github.com/mandelsoft/goutils/matcher"
 	"github.com/spf13/pflag"
 )
 
@@ -15,4 +18,34 @@ type Options interface {
 // purpose of the option, which can be used in the command description.
 type Usage interface {
 	Usage() string
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type OptionsRef[T Options] struct {
+	factory func() T
+	matcher matcher.Matcher[T]
+	Options T
+}
+
+var (
+	_ Options    = (*OptionsRef[Options])(nil)
+	_ Preparable = (*OptionsRef[Options])(nil)
+)
+
+// NewOptionsRef creates a new dynamic reference to Options
+// added once to an OptionSet on the fly during the preparation phase.
+// This can be used to handled Options shared among other
+// aggregating Options structures.
+func NewOptionsRef[T Options](f func() T, check ...matcher.Matcher[T]) *OptionsRef[T] {
+	return &OptionsRef[T]{factory: f, matcher: matcher.And(check...)}
+}
+
+func (o *OptionsRef[T]) AddFlags(fs *pflag.FlagSet) {
+	// just a marker method
+	// Option settings are deferred ro assured options.
+}
+
+func (o *OptionsRef[T]) Prepare(ctx context.Context, opts OptionSet, v PreparationSet) error {
+	return SetAssured(&o.Options, opts, o.factory, o.matcher)
 }
